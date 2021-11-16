0ac6073682464ecea73ee2fd160ba9b159713a0c
Your Name <you@example.com>
2021-11-16 18:44:43 +0800

    first commit

---
+++ javascript.js
@@ -0,0 +1,31 @@
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function () {
+        menu.toggle();
+    });
+    menu.click(function () {
+        menu.toggle();
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) {
+        klikMenu();
+    }
+        
+})
+
+//check lebar
+$(window).resize(function(){
+    var width = $(window).width();
+    if(width > 989){
+        menu.css("display","block");
+    }else{
+        menu.css("display","none");
+    }
+    klikMenu();
+});
